f096d8aeb014a4fb9a3548c7f792fadf9cfcd2c4
Tatsuya Teshima <terima666tatsuya@yahoo.co.jp>
2020-04-19 20:31:27 +0900

    env fix

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -1,17 +1,19 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 var firebaseConfig = {
-  apiKey: process.env.FB_API_KEY,
+  apiKey: process.env.REACT_APP_FB_API_KEY,
   authDomain: "everyday-todo.firebaseapp.com",
   databaseURL: "https://everyday-todo.firebaseio.com",
   projectId: "everyday-todo",
   storageBucket: "everyday-todo.appspot.com",
-  messagingSenderId: process.env.FB_MSG_SENDER_ID,
-  appId: process.env.FB_APP_ID,
-  measurementId: process.env.FB_MEASUREMENT_ID,
+  messagingSenderId: process.env.REACT_APP_FB_MSG_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID,
+  measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
+
 export default firebase;
