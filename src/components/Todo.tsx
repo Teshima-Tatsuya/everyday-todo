a035fdf5846427c2f4218b7dfc28401b2dd72a17
Tatsuya Teshima <terima666tatsuya@yahoo.co.jp>
2020-05-10 14:40:58 +0900

    add complete style

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,10 +2,21 @@ import React, { useContext } from "react";
 import { ITodo } from "../types/todo";
 import { TodoContext } from "../contexts/todo";
 import Checkbox, { CheckboxProps } from "@material-ui/core/Checkbox";
+import ListItemText from "@material-ui/core/ListItemText";
+import styled from "styled-components/macro";
 
 type TodoProps = {
   todo: ITodo;
 };
+
+const TodoText = styled(ListItemText)<{ completed: boolean }>`
+  && {
+    opacity: ${({ completed }) => (completed ? "0.5" : "1.0")};
+    text-decoration: ${({ completed }) =>
+      completed ? "line-through" : "none"};
+    font-color: ${({ completed }) => (completed ? "black" : "gray")};
+  }
+`;
 
 const Todo: React.FC<TodoProps> = (props: TodoProps) => {
   const { todo } = props;
@@ -18,7 +29,7 @@ const Todo: React.FC<TodoProps> = (props: TodoProps) => {
 
   return (
     <>
-      {todo.todo}
+      <TodoText primary={todo.todo} completed={todo.isComplete} />
       <Checkbox checked={todo.isComplete} onChange={handleChange} />
     </>
   );
