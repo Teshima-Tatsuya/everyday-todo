c817999b99624697e78df3b8c285bf5466d23774
Tatsuya Teshima <terima666tatsuya@yahoo.co.jp>
2020-05-10 14:45:06 +0900

    add wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useReducer, useContext } from "react";
-import "./App.css";
 import { UserProvider, UserContext } from "./contexts/user";
 import Main from "./components/Main";
 
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,12 +6,28 @@ import TodoList from "./TodoList";
 import User from "./User";
 import GoogleLogin from "./GoogleLogin";
 import Loading from "./Loading";
+import styled from "styled-components/macro";
+
+const Wrapper = styled.div`
+  & {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: whitesmoke;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 20px;
+  }
+`;
 
 const Main: React.FC = (props) => {
   const { isLoading, user } = useContext(UserContext);
 
   return (
-    <div className="App">
+    <Wrapper>
       {isLoading ? (
         <Loading />
       ) : user ? (
@@ -23,7 +39,7 @@ const Main: React.FC = (props) => {
       ) : (
         <GoogleLogin />
       )}
-    </div>
+    </Wrapper>
   );
 };
 
